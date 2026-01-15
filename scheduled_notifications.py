5f2c358dae4669200f10a92082fb39f8bde870f2
Ulugbek Umaraliyev <94630185+ulugbek101@users.noreply.github.com>
2026-01-15 17:42:53 +0500

    Update scheduled_notifications.py

--- scheduled_notifications.py
+++ scheduled_notifications.py
@@ -73,12 +73,12 @@ async def day_end():
         # Chech if user started working day
         user_id = db.get_user(telegram_id=user.get("telegram_id")).get("id")
         day_id = db.get_day(date.today()).get("id")
-        is_user_worked_today = db.get_attendance(user_id=user_id, day_id=day_id).get("start_time")
-        is_user_finished_already = db.get_attendance(user_id=user_id, day_id=day_id).get("end_time") == None
+        # is_user_worked_today = db.get_attendance(user_id=user_id, day_id=day_id).get("start_time")
+        # is_user_finished_already = db.get_attendance(user_id=user_id, day_id=day_id).get("end_time") == None
 
-        if not is_user_worked_today or is_user_finished_already:
-            fails.append({"first_name": user.get("first_name"), "last_name": user.get("last_name")})
-            continue
+        # if not is_user_worked_today or is_user_finished_already:
+            # fails.append({"first_name": user.get("first_name"), "last_name": user.get("last_name")})
+            # continue
 
         lang = user.get("lang")
 
